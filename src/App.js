903bdbea07e2c56ab0cdd4eba343471eb69ce7f0
HarshitaChoudharyITG <harshitachoudhary@itgeeks.com>
2025-03-03 17:36:22 +0530

    Content Add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <h1 className="heading">QUNATITY BOX</h1>
+    <div className="quantity-box">
+      <button className="quantity-button" name="minus" type="button">-</button>
+      <input type="number" name="quantity" className="quantity-input" min="1" value="1" />
+      <button className="quantity-button" name="plus" type="button">+</button>
     </div>
+    </>
   );
 }
 
